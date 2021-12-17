e156e9907c3c4fdeec9ad4df0a1e74b5c8c921f4
Vedant Agarwal <vedanthello@gmail.com>
2021-12-17 17:29:17 +0530

    Connect to empty Mongo database

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  DB: process.env.APP_DB,
+  CLUSTER_URL: process.env.APP_CLUSTER_URL,
+  DB_NAME: process.env.APP_DB_NAME,
+  DB_USERNAME: process.env.APP_DB_USERNAME,
+  DB_PASSWORD: process.env.APP_DB_PASSWORD,
   PORT: process.env.APP_PORT,
   SECRET: process.env.APP_SECRET
 };
--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const cors = require("cors");
-const exp = require("express");
-const { connect } = require("mongoose");
+const express = require("express");
 const { success, error } = require("consola");
+const { MongoClient } = require('mongodb');
 
 // Bring in the app constants
-const { DB, PORT } = require("./config");
+const { PORT } = require("./config");
 
 // Initialise the application
-const app = exp();
+const app = express();
 
-app.listen(PORT, () => 
-  success({ message: `Server started on Port: ${PORT}`, badge: true })
-);
+// Common middlewares
+app.use(cors());
+app.use(express.json());
+
+// Connect with the Mongo database cluster and start the server
+const { CLUSTER_URL, DB_USERNAME, DB_PASSWORD, DB_NAME } = require("./config");
+const uri = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${CLUSTER_URL}/${DB_NAME}?retryWrites=true&w=majority`; 
+console.log(uri);            
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const startApp = async () => {
+  try {
+    await client.connect();
+
+    success({ 
+      message: `Successfully connected to database cluster \n${CLUSTER_URL}`,
+      badge: true
+    });
+
+    app.listen(PORT, () =>
+      success({ message: `Server started on Port: ${PORT}`, badge: true })
+    );
+
+  }
+
+  catch (err) {
+    error({
+      message: `Unable to connect with database cluster\n${err}`,
+      badge: true
+    });
+    startApp();
+  }
+};
+
+startApp();
